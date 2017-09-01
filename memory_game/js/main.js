9a62e48e192493063514b1ccf341178c5c6a6e16
Kasia <23254155+tripz4fun@users.noreply.github.com>
2017-09-01 01:33:48 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,19 +28,17 @@ var cards = [
 
 cardsInPlay = [];
 
-var checkForMatch = function(){
-    if (cardsInPlay.length === 2) {
-        if (cardsInPlay[0] === cardsInPlay[1]){
-            alert("You found a match!");
-        }
-        else {
+var checkForMatch = function(cardId){
+    if (cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+    }
+    else {
             alert("Sorry, try again");
-        }
     }
 };
 
-var flipCard = function(cardId){
-
+var flipCard = function(){
+    var cardId = this.getAttribute('data-id');
     console.log("User flipped " + cards[cardId].rank);
 
     cardsInPlay.push(cards[cardId].rank);
@@ -48,10 +46,21 @@ var flipCard = function(cardId){
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
 
-    checkForMatch();
+    this.setAttribute('src', cards[cardId].cardImage);
+    if (cardsInPlay.length === 2) {
+        checkForMatch(cardId);
+    };
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function(){
+    for(i = 0; i < cards.length; i++){
+        var cardElement = document.createElement('img');
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+        cardElement.addEventListener('click', flipCard);
+        document.getElementById('game-board').appendChild(cardElement);
+        console.log(i);
+    }
+};
 
-
+createBoard();
